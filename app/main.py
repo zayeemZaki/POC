9f05b9eccf30130c63472415abbc71988bd47f87
zayeemZaki <zayeemzaki45@gmail.com>
2026-01-29 23:22:52 -0500

    fix cors error

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Insurance Claim Agent POC")
 # Allow the Next.js frontend to call the API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
